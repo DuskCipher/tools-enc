907727b6dd5e307f3b867ed12704000e7bf06e74
DuskCipher <aprineads@gmail.com>
2025-07-05 18:12:16 +0700

    Update dusk_cipher.py

--- dusk_cipher.py
+++ dusk_cipher.py
@@ -91,10 +91,10 @@ class DuskCipher:
 
         # Simplified info display
         print(
-            f"{Colors.CYAN}║{Colors.END} {Colors.GRAY}Version: {Colors.GREEN}v{self.version}{Colors.GRAY} │Runs: {Colors.GREEN}{self.total_runs}{Colors.GRAY} │             Status: {Colors.GREEN}{self.status[:15]:<15}{Colors.CYAN}      ║{Colors.END}"
+            f"{Colors.CYAN}║{Colors.END} {Colors.GRAY}Version: {Colors.GREEN}v{self.version}{Colors.GRAY} │Runs: {Colors.GREEN}{self.total_runs}{Colors.GRAY} │             Status: {Colors.GREEN}{self.status[:15]:<15}{Colors.CYAN}       ║{Colors.END}"
         )
         print(
-            f"{Colors.CYAN}║{Colors.END} {Colors.GRAY}Platform: {Colors.YELLOW}{self.platform[:30]:<30}{Colors.GRAY} │ Author: {Colors.YELLOW}{self.author}{Colors.GRAY}{' ' * (10-len(self.author))}{Colors.CYAN}       ║{Colors.END}"
+            f"{Colors.CYAN}║{Colors.END} {Colors.GRAY}Platform: {Colors.YELLOW}{self.platform[:30]:<30}{Colors.GRAY} │ Author: {Colors.YELLOW}{self.author}{Colors.GRAY}{' ' * (10-len(self.author))}{Colors.CYAN}      ║{Colors.END}"
         )
 
         print(f"{Colors.CYAN}{'╚' + '═' * 68 + '╝'}{Colors.END}")
